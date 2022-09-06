a1b47db98eff43bc77851f822425d154816d3410
rodrigolb01 <rodrigolb01@gmail.com>
2022-09-05 22:00:41 -0300

    better code formatting

--- test/test.js
+++ test/test.js
@@ -1,20 +1,21 @@
-const VendingMachine = artifacts.require("VendingMachine");
 const VerifySignature = artifacts.require("VerifySignature");
 
-contract("VendingMachine", (accounts) =>{
+contract("VerifySignature", (accounts) =>{
     const [acc1, acc2] = accounts;
       // run constructor
     beforeEach(async () => {
-        vendingMachine = await VendingMachine.new(); 
         verifySignature = await VerifySignature.new();
     });
 
 
     it('hash message', async ()=>{
+        let msg = "donutz";
+        let recipientAddress = '0x14723A09ACff6D2A60DcdF7aA4AFf308FDDC160C';
+        let signerAddress = '0x44818e00A3E71582858425707746fb7DDFab927e';
         let hash = await verifySignature.getMessageHash(
-            '0x14723A09ACff6D2A60DcdF7aA4AFf308FDDC160C',
+            recipientAddress,
             123,
-            "donutz",
+            msg,
             1);
         let signature = '0x9c9bb7c02118d9197af530d7db6577ce8b6f52200e290e0c42df9db5c966511227117c31c1c7bc420808fa1a22da241de46d7b8eccc9ce0363eb13e51fffe3511b';
         let ethHash = await verifySignature.getEthSignedMessageHash(hash);
@@ -22,19 +23,18 @@ contract("VendingMachine", (accounts) =>{
             ethHash,
             signature);
         let verify = await verifySignature.verify(
-            '0x44818e00A3E71582858425707746fb7DDFab927e',
-            '0x14723A09ACff6D2A60DcdF7aA4AFf308FDDC160C',
+            signerAddress,
+            recipientAddress,
             123,
-            'donutz',
+            msg,
             1,
             signature
         )
-        console.log('original message: "donutz" ')
+        console.log('original message: ' + msg);
         console.log('message hash:' + hash);
+        console.log('personal signature is: ' + signature);
+        console.log('ethereum signature is: ' + ethHash);
         console.log('signer: ' + signer);
         console.log('signature is valid: ' + verify);
-        
-        
     });
-
 })
